adf4cad6694bfa4b90f4372ce94a75df6b673df4
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-12-04 21:02:48 +0800

    got value of form: how many cookies

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+  fortuneCookieForm: FormGroup
+  
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.fortuneCookieForm = this.fb.group({
+      numberOfCookies: this.fb.control(1)
+    })
+  }
+
+  getCookies() {
+    const numOfCookies = parseInt(this.fortuneCookieForm.value['numberOfCookies'])
+    // console.log("🍪", numOfCookies)
+  }
+
 }
